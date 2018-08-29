6a0f6ca0a7f926f152adb81014044e6f61c61ec7
unknown <j.veitchmichaelis@gmail.com>
2018-08-29 14:07:11 +0100

    Initial unit tests for Boson

--- test/test_boson.py
+++ test/test_boson.py
@@ -1,4 +1,19 @@
+import unittest
+import doctest
+
 from flirpy.camera.boson import Boson
 
-with Boson("COM7") as cam:
-    print("Serial: ", cam.get_camera_serial())
+class Test(unittest.TestCase):
+    """Unit tests for Boson."""
+
+    def setUp(self):
+        self.camera = Boson("COM7")
+    
+    def tearDown(self):
+        self.camera.close()
+
+    def get_serial(self):
+        assert(self.camera.get_camera_serial() != 0)
+
+if __name__ == "__main__":
+    unittest.main()
