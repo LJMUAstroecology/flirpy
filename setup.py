b6ee420a29b0aa3b6ef87c83e81175d15643fdc2
unknown <j.veitchmichaelis@gmail.com>
2018-08-29 14:16:15 +0100

    Basic setup.py with implicit namespacing

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup
 setup(
     name='flirpy',
     version='0.0.1',
-    packages=['flirpy',],
+    packages=['flirpy.camera',],
+    author='Josh Veitch-Michaelis',
+    author_email='j.veitchmichaelis@gmail.com',
     license='MIT',
     long_description=open('README.md').read(),
 )
