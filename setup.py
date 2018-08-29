4e317dfd3f36aba4dd9ad8ccb6ce766084594938
unknown <j.veitchmichaelis@gmail.com>
2018-08-29 14:08:40 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='flirpy',
+    version='0.0.1',
+    packages=['flirpy',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
