7eeacd776074bc4c5474900ddec124ce46a11f61
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2019-01-10 17:28:16 +0000

    tests for exiftool

--- test/test_exiftool.py
+++ test/test_exiftool.py
@@ -1,24 +1,5 @@
-import unittest
-import doctest
-import pkg_resources
-import os
-import subprocess
+from flirpy.util.exiftool import Exiftool
 
-class ExifToolTest(unittest.TestCase):
-    """Check for exiftool."""
-
-    def setUp(self):
-        if os.name == "nt":
-            self.exiftool = pkg_resources.resource_filename('flirpy', 'bin/exiftool.exe')
-        else:
-            self.exiftool = pkg_resources.resource_filename('flirpy', 'bin/exiftool')
-    
-    def tearDown(self):
-        pass
-
-    def test_exiftool_exists(self):
-        # Fails with FileNotFoundError
-        subprocess.check_output([self.exiftool])
-
-if __name__ == "__main__":
-    unittest.main()
+def test_exiftool_exists():
+    exiftool = Exiftool()
+    assert(exiftool.check_path() == True)
