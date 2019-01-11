10181768c14bbfecb692e211c67bc94981419058
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2019-01-11 22:50:02 +0000

    add gps test

--- test/test_fff.py
+++ test/test_fff.py
@@ -1,4 +1,25 @@
 from flirpy.io.fff import Fff
+from flirpy.util.exiftool import Exiftool
+import os
 
-def test_fff_read():
-    fff_file = Fff
+class TestFff:
+    def setup_method(self):
+
+        folder = os.path.dirname(os.path.abspath(__file__))
+        self.test_data_fff = os.path.join(folder, "test_data", "frame_000000.fff")
+        self.test_data_meta = os.path.join(folder, "test_data", "frame_000000.txt")
+
+        self.frame = Fff(self.test_data_fff)
+
+    def test_read_image(self):
+        image = self.frame.get_image()
+        assert len(image.shape) == 2
+
+    def test_read_radiometric_image(self):
+        meta = Exiftool().meta_from_file(self.test_data_meta)
+        image = self.frame.get_radiometric_image(meta)
+        assert len(image.shape) == 2
+
+    def test_fff_gps(self):
+        coord =  self.frame.get_gps()
+        assert len(coord) == 9
