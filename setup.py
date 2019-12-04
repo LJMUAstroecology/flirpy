90495d521000a93b105f0935b11840d21367f58f
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2019-12-04 12:56:39 +0000

    update to 0.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import platform
 # Opencv headless not available on ARM platforms, need to manual install
 if platform.machine() in ["arm", "aarch64", "aarch64_be", "armv8b", "armv8l"]:
     install_requires=['pyserial', 'tqdm', 'numpy']
-    
+
     print("System detected as ARM. This library depends on OpenCV, which is not \
            available as a wheel yet so you will need to build from scratch. If you're running \
            aarch64, you can try 'pip install opencv-python-aarch64' but this is not officially supported.")
@@ -18,7 +18,7 @@ __packagename__ = "flirpy"
 
 setup(
     name=__packagename__,
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(),
     author='Josh Veitch-Michaelis',
     author_email='j.veitchmichaelis@gmail.com',
