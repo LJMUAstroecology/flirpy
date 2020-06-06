94c36f4f66f62607484131b1be528bfe27af4e9e
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2020-06-06 13:02:29 +0100

    add dependencies and version bump

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ import platform
 
 # Opencv headless not available on ARM platforms, need to manual install
 if platform.machine() in ["arm", "aarch64", "aarch64_be", "armv8b", "armv8l"]:
-    install_requires=['pyserial', 'tqdm', 'numpy']
+    install_requires=['pyserial', 'tqdm', 'numpy', 'natsort', 'pyudev', 'psutil', 'pyusb', 'pyftdi']
 
     print("System detected as ARM. This library depends on OpenCV, which is not \
            available as a wheel yet so you will need to build from scratch. If you're running \
            aarch64, you can try 'pip install opencv-python-aarch64' but this is not officially supported.")
 else:
-    install_requires=['pyserial', 'opencv-python-headless', 'tqdm', 'numpy', 'pyudev', 'psutil']
+    install_requires=['pyserial', 'opencv-python-headless', 'tqdm', 'numpy', 'pyudev', 'psutil', 'natsort', 'pyusb', 'pyftdi']
 
 __packagename__ = "flirpy"
 
 setup(
     name=__packagename__,
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     author='Josh Veitch-Michaelis',
     author_email='j.veitchmichaelis@gmail.com',
