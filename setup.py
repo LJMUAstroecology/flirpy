ada3e02d61d8baa5d45698e6172ed2369bde9490
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2020-09-30 06:58:13 +0100

    version bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ __packagename__ = "flirpy"
 
 setup(
     name=__packagename__,
-    version='0.1.2',
+    version='0.2.3',
     packages=find_packages(),
     author='Josh Veitch-Michaelis',
     author_email='j.veitchmichaelis@gmail.com',
